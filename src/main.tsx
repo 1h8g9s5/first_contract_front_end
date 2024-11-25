c09b1dd20fca5103e1386fc4295792f7d5fb99d9
1h8g9s5 <1h8g9s5@gmail.com>
2024-11-24 20:00:42 -0500

    TonConnect steps in 5.2 completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
+
+const manifestUrl = "https://raw.githubusercontent.com/1h8g9s5/my-twa/refs/heads/main/public/tonconnect-manifest.json"
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </StrictMode>,
+  </TonConnectUIProvider>,
 )
